640741984af1c4c191a6e43025f9be465a565bce
xiaxiang <xiaxiang@heytea.com>
2024-09-25 20:51:18 +0800

    commit code

--- lib/card.dart
+++ lib/card.dart
@@ -21,18 +21,19 @@ class _CardState extends State<CardItem> {
 
     // var image  = Image(image: AssetImage(widget.classifyValue.imageUrl??""));
     return Card(
+        color: const Color.fromARGB(255, 255, 255, 255), // 背景颜色
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5.0), // 设置圆角半径为 16.0
+          borderRadius: BorderRadius.circular(5.0), // 设置圆角半径为 5.0
         ),
         elevation: 5,
         child: SizedBox(
-          width: screenWidth*widthFactor,
-          height: screenHeight*heightFactor,
+          width: screenWidth * widthFactor,
+          height: screenHeight * heightFactor,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: screenHeight * heightFactor * 0.8,
+                height: screenHeight * heightFactor * 0.7,
                 // child: ConstrainedBox(
                 //   constraints: const BoxConstraints.expand(),
                 //   child: Image(fit: BoxFit.cover,image: AssetImage(widget.classifyValue.imageUrl??"")),
@@ -43,26 +44,92 @@ class _CardState extends State<CardItem> {
                 // child: Image(fit: BoxFit.cover,image: AssetImage(widget.classifyValue.imageUrl??"")),
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.vertical(top: Radius.circular(5.0)), // 圆角
+                    borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(5.0)), // 圆角
                     image: DecorationImage(
                         image: AssetImage(widget.classifyValue.imageUrl ?? ""),
                         fit: BoxFit.cover),
                   ),
                 ),
               ),
-              Row(
-                children: [
-                  SizedBox(
-                    height: screenHeight * heightFactor * 0.2,
-                    // child: Text(widget.classifyValue.title ?? ""),
-                    child: Padding(
-                        padding: const EdgeInsets.only(bottom: 8),
-                        child: Text(widget.classifyValue.title ?? "",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ))),
-                  )
-                ],
+              SizedBox(
+                height: screenHeight * heightFactor * 0.3,
+                width: screenWidth * widthFactor,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                10.0, 10.0, 10.0, 0.0),
+                            child: Text(widget.classifyValue.title ?? "",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13))),
+                        // Align(
+                        //     alignment: Alignment.centerRight,
+                        //     child: Icon(Icons.home)),
+                        const Align(
+                          alignment: Alignment.centerRight,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                                child: Icon(
+                                  Icons.image,
+                                  size: 18,
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                                  child: Text("20",
+                                      style: TextStyle(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey))),
+                              Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 0.0),
+                                child: const Icon(
+                                  Icons.play_circle,
+                                  size: 18,
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(0.0, 10.0, 20.0, 0.0),
+                                  child: Text("5",
+                                      style: TextStyle(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey))),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: screenWidth * widthFactor,
+                          child: Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  10.0, 2.0, 10.0, 0.0),
+                              child: Text(
+                                  overflow: TextOverflow.ellipsis,
+                                  widget.classifyValue.des ?? "",
+                                  maxLines: 3,
+                                  style: const TextStyle(
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 11))),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           ),
--- lib/model/classify_value.dart
+++ lib/model/classify_value.dart
@@ -1,9 +1,10 @@
 class ClassifyValue{
 
-  ClassifyValue(String this.title,String this.imageUrl);
+  ClassifyValue(String this.title,String this.imageUrl,String this.des);
 
   String? title;
   String? imageUrl;
+  String? des;
 
   getTitle()=>this.title;
   getImageUrls()=>this.imageUrl;
