82ac11b88c3d8f6fce9493ddf89035c5aba997e9
xiaxiang <xiaxiang@heytea.com>
2024-10-09 11:22:53 +0800

    commit code

---
+++ lib/state/data_state.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/model/classify_value.dart';
+import 'package:flutter_learn/model/node_value.dart';
+import 'package:flutter_learn/util/data_util.dart';
+
+class DataState with ChangeNotifier {
+  bool init = false;
+  List<ClassifyValue> classifyValues = [];
+  Map<String, List<NodeValue>> nodeMap = {};
+
+  DataState(
+      {List<ClassifyValue>? classifyValues,
+      Map<String, List<NodeValue>>? nodeMap}){
+        load();
+      }
+
+  Future<void> load() async {
+    classifyValues = await DataUtil.readAllClassify();
+    nodeMap = await DataUtil.readAllNode();
+    init = true;
+    notifyListeners();
+  }
+
+ Future<void> addClassify(ClassifyValue classify) async{
+    var newClassifyList =  await DataUtil.insertClassifyFrist(classifyValues, classify);
+    classifyValues = newClassifyList;
+    notifyListeners();
+  }
+
+  Future<void> deleteClassify(ClassifyValue classify) async{
+    ClassifyValue? deleteItem;
+    for (var element in classifyValues) {
+      if(classify.id == element.id){
+        deleteItem = element;
+      }
+    }
+    if(deleteItem!=null){
+      classifyValues.remove(deleteItem);
+    }
+    await DataUtil.saveClassify(classifyValues);
+    notifyListeners();
+  }
+}
